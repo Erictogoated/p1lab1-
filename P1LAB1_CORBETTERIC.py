99db1dfbf04c1af97eb64609830a988a0a1d3a94
Erictogoated <corbette4431@student.faytechcc.edu>
2024-09-21 16:32:10 -0400

    Add files via upload

---
+++ P1LAB1_CORBETTERIC.py
@@ -0,0 +1,12 @@
+
+print ('eric corbett')
+print ('9/19/2024')
+print ('PWM1')
+print('Project is about using python idle')
+
+
+first_name = input("Enter your first_name: ")
+last_name = input("Enter your last_name: ")
+
+print ('Welcome')
+print(f"Hello {first_name} {last_name}! welcome to CTI-110")
